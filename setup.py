7c4b413aacd605cdc904910042e9bdf5b295b4b1
yjjong623 <44912049+yjjong623@users.noreply.github.com>
2019-05-13 17:02:48 -0400

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-
-setup(
-    ext_modules=cythonize(['primes.pyx',        # Cython code file with primes() function
-                           'primes.py'],  # Python code file with primes_python_compiled() function
-                          annotate=True),        # enables generation of the html annotation file
-)
