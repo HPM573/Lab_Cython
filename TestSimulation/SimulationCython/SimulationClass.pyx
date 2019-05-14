0ef77e9ba785e9c393b6507e13de7a3dc51ef330
yjjong623 <44912049+yjjong623@users.noreply.github.com>
2019-05-14 16:40:43 -0400

    Create SimulationClass.pyx

---
+++ TestSimulation/SimulationCython/SimulationClass.pyx
@@ -0,0 +1,14 @@
+import numpy as np
+import cython
+
+
+class Simulation:
+    def __init__(self):
+        self.rng = np.random.RandomState(seed=0)
+        self.sum = 0
+
+    def simulate(self, n_steps):
+
+        for i in range(n_steps):
+            self.sum += self.rng.random_sample()
+            
