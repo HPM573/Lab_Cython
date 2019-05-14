87135c255289ead4cd15997814f0657cbb6be17a
yjjong623 <44912049+yjjong623@users.noreply.github.com>
2019-05-14 10:26:53 -0400

    Update setup.py

--- Primes/setup.py
+++ Primes/setup.py
@@ -4,5 +4,5 @@ from Cython.Build import cythonize
 setup(
     ext_modules=cythonize(['primes.pyx',        # Cython code file with primes() function
                            'primes.py'],  # Python code file with primes_python_compiled() function
-                          annotate=True),        # enables generation of the html annotation file
+                         ),       
 )
