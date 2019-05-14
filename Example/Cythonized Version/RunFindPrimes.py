32629688d468261f083b86127f535558e2db7ae2
yjjong623 <44912049+yjjong623@users.noreply.github.com>
2019-05-14 14:56:38 -0400

    Create RunFindPrimes.py

---
+++ Example/Cythonized Version/RunFindPrimes.py
@@ -0,0 +1,10 @@
+import time
+start = time.time()
+import FindPrimes as cls
+
+myClass = cls.FindPrimes(n=100)
+print(myClass.get_prime_numbers())
+
+end = time.time()
+print("time")
+print(end - start)
