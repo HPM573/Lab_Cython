82d7673ae4b6d6bd2efd5af993eb644ea6e55213
yjjong623 <44912049+yjjong623@users.noreply.github.com>
2019-05-15 14:06:27 -0400

    Create mcy.pyx

---
+++ TestMonteCarlo/scalar_cython/mcy.pyx
@@ -0,0 +1,17 @@
+import random
+
+def dice6_py(N, ndice, nsix):
+    M = 0                     # no of successful events
+    for i in range(N):        # repeat N experiments
+        six = 0               # how many dice with six eyes?
+        for j in range(ndice):
+            r = random.randint(1, 6)  # roll die no. j
+            if r == 6:
+               six += 1
+        if six >= nsix:       # successful event?
+            M += 1
+    p = float(M)/N
+    return p
+
+
+
