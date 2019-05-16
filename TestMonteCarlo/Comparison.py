7d223d66d70005d9d3e2524061bfc3885b7c6471
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2019-05-16 13:08:53 -0400

    adding windows instruction

--- TestMonteCarlo/Comparison.py
+++ TestMonteCarlo/Comparison.py
@@ -1,24 +1,26 @@
 import time
-from scalar_python import mpy as regpy
-from scalar_cython import mcy as regcy
-from better_cython import bcy as betcy
+from TestMonteCarlo.scalar_python import mpy as regpy
+from TestMonteCarlo.scalar_cython import mcy as regcy
+from TestMonteCarlo.better_cython import bcy as betcy
+
+N = 100000
 
 start = time.time()
-myClass = regpy.dice6_py(N=100, ndice=10, nsix=10)
+myClass = regpy.dice6_py(N=N, ndice=10, nsix=10)
 #print(myClass)
 end = time.time()
 print("time in Scalar Python")
 print(end - start)
 
 start = time.time()
-myClass = regcy.dice6_py(N=100, ndice=10, nsix=10)
+myClass = regcy.dice6_py(N=N, ndice=10, nsix=10)
 #print(myClass)
 end = time.time()
 print("time in Scalar Cython")
 print(end - start)
 
 start = time.time()
-myClass = betcy.dice6_cy1(N=100, ndice=10, nsix=10)
+myClass = betcy.dice6_cy1(N=N, ndice=10, nsix=10)
 #print(myClass)
 end = time.time()
 print("time in Better Cython")
