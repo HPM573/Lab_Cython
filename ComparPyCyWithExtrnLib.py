4aa28fe8b3b87fee35a0d6d0f5aff21632afa242
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2019-05-31 12:18:19 -0400

    adding sim with external support

--- ComparPyCyWithExtrnLib.py
+++ ComparPyCyWithExtrnLib.py
@@ -1,11 +1,31 @@
-import PyExternalPy.SimClasses as P
+import PyExternalPy.SimClasses as PP
+import PyExternalCy.SimClasses as PC
+import CyExternalCy.SimClasses as CC
 import time
 
 # python simulation
 ppStart = time.time()
-ppModel = P.MultiSim()
+ppModel = PP.MultiSim()
 ppModel.simulate(n_steps=1000, n_iterations=100)
 ppModel.export_results()
 ppEnd = time.time()
 
 print("Python simulation time with external support in Python: ", ppEnd - ppStart)
+
+# cython simulation
+pcStart = time.time()
+pcModel = PC.MultiSim()
+pcModel.simulate(n_steps=1000, n_iterations=100)
+pcModel.export_results()
+pcEnd = time.time()
+
+print("Python simulation time with external support in Cython: ", pcEnd - pcStart)
+
+# cython simulation
+ccStart = time.time()
+ccModel = CC.MultiSim()
+ccModel.simulate(n_steps=1000, n_iterations=100)
+ccModel.export_results()
+ccEnd = time.time()
+
+print("Cython simulation time with external support in Cython: ", ccEnd - ccStart)
--- PyExternalPy/RunSim.py
+++ PyExternalPy/RunSim.py
@@ -1,6 +1,4 @@
 import PyExternalPy.SimClasses as PP
-import PyExternalCy.SimClasses as PC
-import CyExternalCy.SimClasses as CC
 import time
 
 # python simulation
@@ -11,21 +9,3 @@ ppModel.export_results()
 ppEnd = time.time()
 
 print("Python simulation time with external support in Python: ", ppEnd - ppStart)
-
-# cython simulation
-pcStart = time.time()
-pcModel = PC.MultiSim()
-pcModel.simulate(n_steps=1000, n_iterations=100)
-pcModel.export_results()
-pcEnd = time.time()
-
-print("Python simulation time with external support in Cython: ", pcEnd - pcStart)
-
-# cython simulation
-ccStart = time.time()
-ccModel = CC.MultiSim()
-ccModel.simulate(n_steps=1000, n_iterations=100)
-ccModel.export_results()
-ccEnd = time.time()
-
-print("Cython simulation time with external support in Cython: ", ccEnd - ccStart)
